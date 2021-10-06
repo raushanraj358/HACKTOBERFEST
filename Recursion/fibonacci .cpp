b579cd27a6c7d66799c84aef11b111a9136ebf7b
Divyanshi-03 <85453778+Divyanshi-03@users.noreply.github.com>
2021-10-07 00:42:21 +0530

    Create fibonacci .cpp

---
+++ Recursion/fibonacci .cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if(n==0)
+    return 0;
+    else if(n==1)
+    return 1;
+    else 
+    return fibonacci(n-1) + fibonacci(n-2);
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fibonacci(n) ;
+
+    return 0;
+}
