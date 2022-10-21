3f3ddc963a00c06069e67f25c50541003a01b8ce
TanCodes <barvitanmay20@gmail.com>
2022-10-21 21:12:45 +0530

    Added all the python code in one py dsa folder and also added the remove the duplicates in python

---
+++ Python DSA/remove_duplicates.py
@@ -0,0 +1,15 @@
+""" Remove duplicates """
+test_list = [1, 2, 4, 3, 3, 2, 5, 7]
+print(f"original list {test_list}")
+
+res = []
+for i in test_list:
+    if i not in res:
+        res.append(i)
+
+print(str(res))
+
+""" Remove duplicates using Set."""
+x = set(test_list)
+# It also arranges in ascending order
+print(list(x))
